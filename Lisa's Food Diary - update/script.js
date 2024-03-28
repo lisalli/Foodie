d6d9b19ea23b40351583640ab12e9cb847541fc5
lisalli <lil970@newschool.edu>
2024-03-28 19:27:04 -0400

    final upload

---
+++ Lisa's Food Diary - update/script.js
@@ -0,0 +1,482 @@
+let allData= [
+  {
+      "navi":"il Fresco", 
+      "horizontalImg":"HORIZONTAL/1.JPG", 
+      "city":"Orangeburg, NY",
+      "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+      "cuisine":"Italian",
+      "month":"2023.11.05"
+  },
+  {
+      "navi":"Valley Rock Lodge", 
+      "horizontalImg":"HORIZONTAL/2.JPG", 
+      "city":"Sloatsburg, NY",
+      "taste":"&#9733; &#9733; &#9734; &#9734; &#9734;",
+      "cuisine":"American",
+      "month":"2023.11.06"
+  },
+  {
+      "navi":"53", 
+      "horizontalImg":"HORIZONTAL/3.JPG", 
+      "city":"New York, NY",
+      "taste":"&#9733; &#9733; &#9733; &#9733; &#9734;",
+      "cuisine":"Chinese",
+      "month":"2023.11.17"
+  },
+  {
+      "navi":"Jungsik", 
+      "horizontalImg":"HORIZONTAL/4.JPG", 
+      "city":"Tribeca, NY",
+      "taste":"&#9733; &#9733; &#9733; &#9733; &#9734;",
+      "cuisine":"Korean, Contemporary",
+      "month":"2023.11.18"
+  },
+  {
+      "navi":"Blue Hill", 
+      "horizontalImg":"HORIZONTAL/5.JPG", 
+      "city":"Greenwich, NY",
+      "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+      "cuisine":"American",
+      "month":"2023.11.20"
+  },
+  {
+      "navi":"Mauka", 
+      "horizontalImg":"HORIZONTAL/6.JPG", 
+      "city":"Cusco, Peru",
+      "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+      "cuisine":"Peruvian",
+      "month":"2023.11.22"
+  },
+  {
+      "navi":"Wayra", 
+      "horizontalImg":"HORIZONTAL/7.JPG", 
+      "city":"Cusco, Peru",
+      "taste":"&#9733; &#9733; &#9734; &#9734; &#9734;",
+      "cuisine":"Peruvian",
+      "month":"2023.11.23"
+  },
+  {
+      "navi":"MIL", 
+      "horizontalImg":"HORIZONTAL/8.JPG", 
+      "city":"Cusco, Peru",
+      "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+      "cuisine":"Peruvian",
+      "month":"2023.11.24"
+  },
+  {
+      "navi":"Central", 
+      "horizontalImg":"HORIZONTAL/9.JPG", 
+      "city":"Lima, Peru",
+      "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+      "cuisine":"Peruvian",
+      "month":"2023.11.25"
+  },
+  {
+      "navi":"Frevo", 
+      "horizontalImg":"HORIZONTAL/10.JPG", 
+      "city":"Greenwich, NY",
+      "taste":"&#9733; &#9733; &#9733; &#9733; &#9734;",
+      "cuisine":"Contemporary",
+      "month":"2023.12.09"
+  },
+  {
+      "navi":"Akira Back", 
+      "horizontalImg":"HORIZONTAL/11.JPG", 
+      "city":"Toronto, Canada",
+      "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+      "cuisine":"Japanese, Contemporary",
+      "month":"2023.12.15"
+  },
+  {
+      "navi":"Don Alfonso 1890", 
+      "horizontalImg":"HORIZONTAL/12.JPG", 
+      "city":"Toronto, Canada",
+      "taste":"&#9733; &#9733; &#9733; &#9733; &#9734;",
+      "cuisine":"Italian",
+      "month":"2023.12.16"
+  },
+  {
+      "navi":"Hubin 28", 
+      "horizontalImg":"HORIZONTAL/13.JPG", 
+      "city":"Hangzhou, China",
+      "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+      "cuisine":"Chinese",
+      "month":"2023.12.20"
+  },
+  {
+      "navi":"璞珩", 
+      "horizontalImg":"HORIZONTAL/14.JPG", 
+      "city":"Hangzhou, China",
+      "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+      "cuisine":"Chinese",
+      "month":"2023.12.23"
+  },
+  {
+      "navi":"玉玲珑", 
+      "horizontalImg":"HORIZONTAL/15.JPG", 
+      "city":"Hangzhou, China",
+      "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+      "cuisine":"Chinese",
+      "month":"2023.12.27"
+  },
+  {
+      "navi":"桂语山房", 
+      "horizontalImg":"HORIZONTAL/16.JPG", 
+      "city":"Hangzhou, China",
+      "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+      "cuisine":"Chinese",
+      "month":"2024.01.05"
+  },
+  {
+      "navi":"滟设", 
+      "horizontalImg":"HORIZONTAL/17.JPG", 
+      "city":"Chongqing, China",
+      "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+      "cuisine":"Chinese",
+      "month":"2024.01.12"
+  },
+  {
+      "navi":"席作", 
+      "horizontalImg":"HORIZONTAL/18.JPG", 
+      "city":"Shanghai, China",
+      "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+      "cuisine":"Chinese",
+      "month":"2024.01.17"
+  },
+  {
+      "navi":"Kono", 
+      "horizontalImg":"HORIZONTAL/19.JPG", 
+      "city":"New York, NY",
+      "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+      "cuisine":"Japanese",
+      "month":"2024.02.02"
+  },
+  {
+      "navi":"Genesis House", 
+      "horizontalImg":"HORIZONTAL/20.JPG", 
+      "city":"New York, NY",
+      "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+      "cuisine":"Korean",
+      "month":"2024.02.03"
+  },
+  {
+      "navi":"Blue Hill at Stone Barns", 
+      "horizontalImg":"HORIZONTAL/21.JPG", 
+      "city":"Tarrytown, NY",
+      "taste":"&#9733; &#9733; &#9733; &#9733; &#9734;",
+      "cuisine":"American, Contemporary",
+      "month":"2024.02.04"
+  },
+  {
+      "navi":"Soogil", 
+      "horizontalImg":"HORIZONTAL/22.JPG", 
+      "city":"New York, NY",
+      "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+      "cuisine":"Korean",
+      "month":"2024.02.09"
+  },
+  {
+      "navi":"Per Se", 
+      "horizontalImg":"HORIZONTAL/23.JPG", 
+      "city":"New York, NY",
+      "taste":"&#9733; &#9733; &#9733; &#9733; &#9734;",
+      "cuisine":"French, Contemporary",
+      "month":"2024.02.14"
+  },
+  {
+      "navi":"Maison Premiere", 
+      "horizontalImg":"HORIZONTAL/24.JPG", 
+      "city":"Brooklyn, NY",
+      "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+      "cuisine":"Oyster Bar",
+      "month":"2024.02.17"
+  },
+  {
+      "navi":"Restaurant Freisinger Holf", 
+      "horizontalImg":"HORIZONTAL/25.JPG", 
+      "city":"Munich, Germany",
+      "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+      "cuisine":"German",
+      "month":"2024.02.25"
+  },
+  {
+      "navi":"Souvenir Ghent", 
+      "horizontalImg":"HORIZONTAL/26.JPG", 
+      "city":"Ghent, Belgium",
+      "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+      "cuisine":"Belgium, Contemporary",
+      "month":"2024.02.26"
+  },
+  {
+      "navi":"Bistro du Nord ", 
+      "horizontalImg":"HORIZONTAL/27.JPG", 
+      "city":"Antwerp, Belgium",
+      "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+      "cuisine":"Belgium, Contemporary",
+      "month":"2024.02.27"
+  },
+  {
+      "navi":"Nuance", 
+      "horizontalImg":"HORIZONTAL/28.JPG", 
+      "city":"Duffel, Belgium",
+      "taste":"&#9733; &#9733; &#9733; &#9733; &#9734;",
+      "cuisine":"Belgium, Contemporary",
+      "month":"2024.02.27"
+  },
+  {
+      "navi":"La Villa in the Sky", 
+      "horizontalImg":"HORIZONTAL/29.JPG", 
+      "city":"Brussel, Belgium",
+      "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+      "cuisine":"Belgium, Contemporary",
+      "month":"2024.02.28"
+  },
+  {
+      "navi":"Knife & Spoon", 
+      "horizontalImg":"HORIZONTAL/30.JPG", 
+      "city":"Orlando, US",
+      "taste":"&#9733; &#9733; &#9734; &#9734; &#9734;",
+      "cuisine":"American",
+      "month":"2024.03.01"
+  }
+]
+
+document.addEventListener('wheel', function(event) {
+  if (event.deltaY > 0) { 
+    document.getElementById('swipePanel').style.top = '-100%'; 
+  }
+}, { passive: true });
+
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+let touchStartY = 0;
+let touchEndY = 0;
+
+function handleTouchStart(event) {
+  touchStartY = event.touches[0].clientY;
+}                                            
+
+function handleTouchMove(event) {
+  touchEndY = event.touches[0].clientY;
+  if (touchStartY < touchEndY) { 
+    document.getElementById('swipePanel').style.top = '-100%'; 
+  }
+}
+
+let galleryContainer = document.querySelector('.gallery-container');
+
+let galleryData = allData
+function createGallery(){
+  let elements = document.querySelectorAll('.gallery-item');
+  elements.forEach(function(element) {
+      element.parentNode.removeChild(element);
+  });
+  galleryData.forEach(function(item) {
+    let link = document.createElement('a');
+    let id = item.horizontalImg.split('/')[1].split('.')[0];
+    link.href = `subPage.html?id=${id}`;
+    link.className = 'gallery-item';
+    
+    let img = document.createElement('img');
+    img.onmouseover = function(event) {
+      moveImage(event);
+    };
+    img.onmouseout = function(event) {
+        resetImage(event);
+    };
+    img.src = item.horizontalImg;
+  
+
+    link.appendChild(img);
+
+    galleryContainer.appendChild(link);
+  });
+}
+let windowDistance = 140
+let phoneDistance = 42
+let distance = windowDistance
+let blurNumber = 1
+let galleryItems = document.querySelectorAll('.gallery-item');
+let length = galleryItems.length
+initGallery()
+function initGallery(){
+  createGallery()
+  galleryItems = document.querySelectorAll('.gallery-item');
+  let z = distance * 2
+  galleryItems.forEach(item => {
+      item.style.transform = `translateZ(${z}px) translateY(${z}px)`;
+      item.style.zIndex = 9999 - Math.ceil(z * 10);
+      item.style.filter =`blur(${Math.max(0,distance- z) / distance * blurNumber}px)`
+      if (z < -distance * 2){
+          item.style.opacity = 0;
+      }
+      z -= distance
+  });
+  
+}
+  
+  document.addEventListener('wheel', function(event) {
+    let delta = Math.sign(event.deltaY);
+    galleryItems = document.querySelectorAll('.gallery-item');
+    length = galleryItems.length
+    galleryItems.forEach(item => {
+      let z = item.style.transform.match(/translateZ\((-?\d+)px\)/);
+      if (z) {
+        let op = item.style.opacity
+        z = parseInt(z[1]);
+        let newZ = z - delta * (distance / 3);
+        if (newZ == (distance * 2 - length * distance)){
+            newZ = distance * 2
+            op = 1
+        }
+        if(newZ <= -distance * 2){
+            if (newZ == -(distance * 3 + (distance / 3))){
+                op = 0
+            }else if (newZ == -(distance * 3 + (distance / 3 * 2))){
+                op = 0.3
+            }else if(newZ == -distance * 4){
+                op = 0;
+            }else if(newZ == -distance * 3){
+                op = 1
+        }
+        }else if(newZ > distance * 2){
+            if(newZ == distance * 2 + distance / 3){
+                op = 0.6
+            }else if (newZ == distance * 2 + distance / 3 * 2){
+                op = 0.3
+            }else{
+                op = 0;
+                newZ = distance * 3 - length * distance
+                if(newZ>=0){
+                  op = 1
+                }
+            }
+        }
+        item.style.transform = `translateZ(${newZ}px) translateY(${newZ}px)`;
+        item.style.filter = `blur(${Math.max(0,distance - z) / distance * blurNumber}px)`
+        let zIndex = 9999 - Math.ceil(newZ * 10);
+        item.style.zIndex = zIndex;
+        item.style.opacity = op;
+      }
+    });
+  });
+let months= {
+  "all": true,
+  'november': true,
+  'december': true,
+  'january': true,
+  'february': true,
+  'march': true
+}
+let int2str = {
+  11:"november",
+  12:"december",
+  1:"january",
+  2:"february",
+  3:"march"
+}
+function chooseMonth(id){
+  if(id == 'all'){
+    for (let key in months) {
+      months[key] = true
+      let monthElement = document.getElementById(key)
+      monthElement.classList.add('selected');
+      
+    }
+    galleryData = []
+    allData.forEach(item => {
+      galleryData.push(item)
+    })
+  }else{
+    months[id] = !months[id]
+    if(months[id] == false){
+      months['all'] = false
+      let allElement = document.getElementById('all')
+      allElement.classList.remove('selected');
+      let monthElement = document.getElementById(id)
+      monthElement.classList.remove('selected');
+    }else{
+      let monthElement = document.getElementById(id)
+      monthElement.classList.add('selected');
+      if(months['november'] && months['december'] && months['january'] && months['february'] && months['march']){
+        let allElement = document.getElementById('all')
+        allElement.classList.add('selected');
+      }
+    }
+    galleryData = []
+    allData.forEach(item => {
+      console.log(int2str[parseInt(item.month.split('.')[1])])
+      if (months[int2str[parseInt(item.month.split('.')[1])]] == true){
+        galleryData.push(item)
+      }
+    })
+  }
+  initGallery()
+}
+function selectImg(id){
+  if(id == 'november'){
+    galleryData = []
+    allData.forEach(item =>{
+      if (parseInt(item.month.split('.')[1])){
+        galleryData.appendChild(item)
+      }
+    })
+  }else if (id == 'december'){
+    galleryData = []
+    allData.forEach(item =>{
+      if (parseInt(item.month.split('.')[1]) == 12){
+        galleryData.appendChild(item)
+      }
+    })
+  }else if (id == 'january'){
+    galleryData = []
+    allData.forEach(item =>{
+      if (parseInt(item.month.split('.')[1]) == 1){
+        galleryData.appendChild(item)
+      }
+    })
+  }else if (id == 'february'){
+    galleryData = []
+    allData.forEach(item =>{
+      if (parseInt(item.month.split('.')[1]) == 2){
+        galleryData.appendChild(item)
+      }
+    })
+  }else{
+    galleryData = all
+  }
+  initGallery()
+}
+
+
+function moveImage(event) {
+  let item = event.target;
+  let parentElement = item.parentElement;
+  console.log(parentElement.style.transform)
+  let newZ = parseInt(parentElement.style.transform.match(/translateZ\((-?\d+)px\)/)[1])
+  parentElement.style.transform = `translateZ(${newZ}px) translateY(${newZ - 30}px)`
+  
+}
+
+function resetImage(event) {
+  let item = event.target;
+  let parentElement = item.parentElement;
+  let newZ = parseInt(parentElement.style.transform.match(/translateZ\((-?\d+)px\)/)[1])
+  parentElement.style.transform = `translateZ(${newZ}px) translateY(${newZ + 30}px)`
+}
+
+
+function setDistanceBasedOnWidth() {
+  let screenWidth = window.innerWidth;
+
+  
+  distance = parseInt(((screenWidth - 410) / (1920 - 410) * (windowDistance - phoneDistance) + phoneDistance) / 3) * 3
+  console.log(screenWidth, distance)
+  initGallery()
+}
+
+setDistanceBasedOnWidth();
+
+window.addEventListener('resize', setDistanceBasedOnWidth);
---
+++ Lisa's Food Diary - update/subPage.js
@@ -0,0 +1,383 @@
+let allData= [
+    {
+        "navi":"il Fresco", 
+        "horizontalImg":"HORIZONTAL/1.JPG", 
+        "city":"Orangeburg, NY",
+        "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+        "cuisine":"Italian",
+        "month":"2023.11.05"
+    },
+    {
+        "navi":"Valley Rock Lodge", 
+        "horizontalImg":"HORIZONTAL/2.JPG", 
+        "city":"Sloatsburg, NY",
+        "taste":"&#9733; &#9733; &#9734; &#9734; &#9734;",
+        "cuisine":"American",
+        "month":"2023.11.06"
+    },
+    {
+        "navi":"53", 
+        "horizontalImg":"HORIZONTAL/3.JPG", 
+        "city":"New York, NY",
+        "taste":"&#9733; &#9733; &#9733; &#9733; &#9734;",
+        "cuisine":"Chinese",
+        "month":"2023.11.17"
+    },
+    {
+        "navi":"Jungsik", 
+        "horizontalImg":"HORIZONTAL/4.JPG", 
+        "city":"Tribeca, NY",
+        "taste":"&#9733; &#9733; &#9733; &#9733; &#9734;",
+        "cuisine":"Korean, Contemporary",
+        "month":"2023.11.18"
+    },
+    {
+        "navi":"Blue Hill", 
+        "horizontalImg":"HORIZONTAL/5.JPG", 
+        "city":"Greenwich, NY",
+        "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+        "cuisine":"American",
+        "month":"2023.11.20"
+    },
+    {
+        "navi":"Mauka", 
+        "horizontalImg":"HORIZONTAL/6.JPG", 
+        "city":"Cusco, Peru",
+        "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+        "cuisine":"Peruvian",
+        "month":"2023.11.22"
+    },
+    {
+        "navi":"Wayra", 
+        "horizontalImg":"HORIZONTAL/7.JPG", 
+        "city":"Cusco, Peru",
+        "taste":"&#9733; &#9733; &#9734; &#9734; &#9734;",
+        "cuisine":"Peruvian",
+        "month":"2023.11.23"
+    },
+    {
+        "navi":"MIL", 
+        "horizontalImg":"HORIZONTAL/8.JPG", 
+        "city":"Cusco, Peru",
+        "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+        "cuisine":"Peruvian",
+        "month":"2023.11.24"
+    },
+    {
+        "navi":"Central", 
+        "horizontalImg":"HORIZONTAL/9.JPG", 
+        "city":"Lima, Peru",
+        "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+        "cuisine":"Peruvian",
+        "month":"2023.11.25"
+    },
+    {
+        "navi":"Frevo", 
+        "horizontalImg":"HORIZONTAL/10.JPG", 
+        "city":"Greenwich, NY",
+        "taste":"&#9733; &#9733; &#9733; &#9733; &#9734;",
+        "cuisine":"Contemporary",
+        "month":"2023.12.09"
+    },
+    {
+        "navi":"Akira Back", 
+        "horizontalImg":"HORIZONTAL/11.JPG", 
+        "city":"Toronto, Canada",
+        "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+        "cuisine":"Japanese, Contemporary",
+        "month":"2023.12.15"
+    },
+    {
+        "navi":"Don Alfonso 1890", 
+        "horizontalImg":"HORIZONTAL/12.JPG", 
+        "city":"Toronto, Canada",
+        "taste":"&#9733; &#9733; &#9733; &#9733; &#9734;",
+        "cuisine":"Italian",
+        "month":"2023.12.16"
+    },
+    {
+        "navi":"Hubin 28", 
+        "horizontalImg":"HORIZONTAL/13.JPG", 
+        "city":"Hangzhou, China",
+        "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+        "cuisine":"Chinese",
+        "month":"2023.12.20"
+    },
+    {
+        "navi":"璞珩", 
+        "horizontalImg":"HORIZONTAL/14.JPG", 
+        "city":"Hangzhou, China",
+        "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+        "cuisine":"Chinese",
+        "month":"2023.12.23"
+    },
+    {
+        "navi":"玉玲珑", 
+        "horizontalImg":"HORIZONTAL/15.JPG", 
+        "city":"Hangzhou, China",
+        "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+        "cuisine":"Chinese",
+        "month":"2023.12.27"
+    },
+    {
+        "navi":"桂语山房", 
+        "horizontalImg":"HORIZONTAL/16.JPG", 
+        "city":"Hangzhou, China",
+        "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+        "cuisine":"Chinese",
+        "month":"2024.01.05"
+    },
+    {
+        "navi":"滟设", 
+        "horizontalImg":"HORIZONTAL/17.JPG", 
+        "city":"Chongqing, China",
+        "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+        "cuisine":"Chinese",
+        "month":"2024.01.12"
+    },
+    {
+        "navi":"席作", 
+        "horizontalImg":"HORIZONTAL/18.JPG", 
+        "city":"Shanghai, China",
+        "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+        "cuisine":"Chinese",
+        "month":"2024.01.17"
+    },
+    {
+        "navi":"Kono", 
+        "horizontalImg":"HORIZONTAL/19.JPG", 
+        "city":"New York, NY",
+        "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+        "cuisine":"Japanese",
+        "month":"2024.02.02"
+    },
+    {
+        "navi":"Genesis House", 
+        "horizontalImg":"HORIZONTAL/20.JPG", 
+        "city":"New York, NY",
+        "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+        "cuisine":"Korean",
+        "month":"2024.02.03"
+    },
+    {
+        "navi":"Blue Hill at Stone Barns", 
+        "horizontalImg":"HORIZONTAL/21.JPG", 
+        "city":"Tarrytown, NY",
+        "taste":"&#9733; &#9733; &#9733; &#9733; &#9734;",
+        "cuisine":"American, Contemporary",
+        "month":"2024.02.04"
+    },
+    {
+        "navi":"Soogil", 
+        "horizontalImg":"HORIZONTAL/22.JPG", 
+        "city":"New York, NY",
+        "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+        "cuisine":"Korean",
+        "month":"2024.02.09"
+    },
+    {
+        "navi":"Per Se", 
+        "horizontalImg":"HORIZONTAL/23.JPG", 
+        "city":"New York, NY",
+        "taste":"&#9733; &#9733; &#9733; &#9733; &#9734;",
+        "cuisine":"French, Contemporary",
+        "month":"2024.02.14"
+    },
+    {
+        "navi":"Maison Premiere", 
+        "horizontalImg":"HORIZONTAL/24.JPG", 
+        "city":"Brooklyn, NY",
+        "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+        "cuisine":"Oyster Bar",
+        "month":"2024.02.17"
+    },
+    {
+        "navi":"Restaurant Freisinger Holf", 
+        "horizontalImg":"HORIZONTAL/25.JPG", 
+        "city":"Munich, Germany",
+        "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+        "cuisine":"German",
+        "month":"2024.02.25"
+    },
+    {
+        "navi":"Souvenir Ghent", 
+        "horizontalImg":"HORIZONTAL/26.JPG", 
+        "city":"Ghent, Belgium",
+        "taste":"&#9733; &#9733; &#9733; &#9734; &#9734;",
+        "cuisine":"Belgium, Contemporary",
+        "month":"2024.02.26"
+    },
+    {
+        "navi":"Bistro du Nord ", 
+        "horizontalImg":"HORIZONTAL/27.JPG", 
+        "city":"Antwerp, Belgium",
+        "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+        "cuisine":"Belgium, Contemporary",
+        "month":"2024.02.27"
+    },
+    {
+        "navi":"Nuance", 
+        "horizontalImg":"HORIZONTAL/28.JPG", 
+        "city":"Duffel, Belgium",
+        "taste":"&#9733; &#9733; &#9733; &#9733; &#9734;",
+        "cuisine":"Belgium, Contemporary",
+        "month":"2024.02.27"
+    },
+    {
+        "navi":"La Villa in the Sky", 
+        "horizontalImg":"HORIZONTAL/29.JPG", 
+        "city":"Brussel, Belgium",
+        "taste":"&#9733; &#9733; &#9733; &#9733; &#9733;",
+        "cuisine":"Belgium, Contemporary",
+        "month":"2024.02.28"
+    },
+    {
+        "navi":"Knife & Spoon", 
+        "horizontalImg":"HORIZONTAL/30.JPG", 
+        "city":"Orlando, US",
+        "taste":"&#9733; &#9733; &#9734; &#9734; &#9734;",
+        "cuisine":"American",
+        "month":"2024.03.01"
+    }
+]
+let imgCount = [1,1,1,5,1,6,7,7,5,4,3,4,1,1,1,9,2,8,4,1,9,1,4,1,1,3,3,2,4,4]
+let params = new URLSearchParams(window.location.search);
+let id = params.get('id');
+
+for (let key in allData[id - 1]){
+    if (key != 'horizontalImg'){
+        let element = document.getElementById(key);
+        element.innerHTML = allData[id - 1][key]
+    }
+    
+}
+
+let nextElement = document.getElementById('next');
+if (id == allData.length){
+    nextElement.href = 'subPage.html?id=1'
+}else{
+    nextElement.href = `subPage.html?id=${Number(id) + 1}`
+}
+let backElement = document.getElementById('back');
+if (id == 1){
+    backElement.href = `subPage.html?id=${allData.length}`
+}else{
+    backElement.href = `subPage.html?id=${Number(id) - 1}`
+}
+
+
+const imgS = imgCount[Number(id) -1];
+const MAX_VISIBILITY = 3;
+let active = 2;
+
+function createimg(imgUrl) {
+  const imgContainer = document.createElement('div');
+  imgContainer.className = 'img-container';
+  imgContainer.style.display = 'none'; // Initially hide all imgs
+  const img = document.createElement('img');
+  img.classList.add('img')
+  console.log(imgUrl)
+  img.src = imgUrl;
+  imgContainer.appendChild(img);
+  return imgContainer;
+}
+
+function renderCarousel() {
+  const carousel = document.getElementById('carousel');
+  carousel.innerHTML = ''; 
+  if(imgS == 1){
+    active=0
+    const imgUrl = `VERTICAL/${id}.JPG`; // Adjust path to your image
+    const img = createimg(imgUrl);
+    carousel.appendChild(img);
+  }else{
+    for (let i = 0; i < imgS; i++) {
+        const imgUrl = `VERTICAL/${id}_${i}.JPG`; // Adjust path to your image
+        const img = createimg(imgUrl);
+        carousel.appendChild(img);
+      }
+  }
+  
+  setActive(active); // Show active img
+}
+
+function setActive() {
+  const imgs = document.querySelectorAll('.img-container');
+  imgs.forEach((img, i) => {
+    const offset = (active - i) / 3;
+    const absOffset = Math.abs(active - i) / 3;
+    img.style.setProperty('--active', i === active ? 1 : 0);
+    img.style.setProperty('--offset', (active - i) / 3);
+    img.style.setProperty('--direction', Math.sign(active - i));
+    img.style.setProperty('--abs-offset', absOffset);
+    img.style.opacity = Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1';
+    img.style.display = Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block';
+    img.style.pointerEvents = active === i ? 'auto' : 'none';
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderCarousel();
+  if(imgS > 1){
+    const leftSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    // 设置 rightSvg 元素的属性
+    leftSvg.setAttribute("stroke", "currentColor");
+    leftSvg.setAttribute("fill", "currentColor");
+    leftSvg.setAttribute("stroke-width", "0");
+    leftSvg.setAttribute("version", "1.2");
+    leftSvg.setAttribute("baseProfile", "tiny");
+    leftSvg.setAttribute("viewBox", "0 0 24 24");
+    leftSvg.setAttribute("height", "5vh");
+    leftSvg.setAttribute("width", "6vh");
+    leftSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    // 创建 path 元素
+    const leftPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+    // 设置 path 元素的 d 属性
+    leftPath.setAttribute("d", "M13 20c-.802 0-1.555-.312-2.122-.879l-7.121-7.121 7.122-7.121c1.133-1.133 3.11-1.133 4.243 0 .566.566.878 1.32.878 2.121s-.312 1.555-.879 2.122l-2.878 2.878 2.878 2.879c.567.566.879 1.32.879 2.121s-.312 1.555-.879 2.122c-.566.566-1.319.878-2.121.878zm-6.415-8l5.708 5.707c.378.378 1.037.377 1.414 0 .189-.189.293-.439.293-.707s-.104-.518-.293-.707l-4.292-4.293 4.292-4.293c.189-.189.293-.44.293-.707s-.104-.518-.293-.707c-.378-.379-1.037-.378-1.414-.001l-5.708 5.708z");
+
+    // 将 path 元素添加到 rightSvg 元素中
+    leftSvg.appendChild(leftPath);
+    // const rightButton = document.createElement('button');
+    leftSvg.setAttribute("class", "nav left");
+    leftSvg.addEventListener('click', () => {
+        if (active > 0) {
+            console.log()
+            active--;
+            setActive();
+        }
+    });
+    const rightSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    // 设置 rightSvg 元素的属性
+    rightSvg.setAttribute("stroke", "currentColor");
+    rightSvg.setAttribute("fill", "currentColor");
+    rightSvg.setAttribute("stroke-width", "0");
+    rightSvg.setAttribute("version", "1.2");
+    rightSvg.setAttribute("baseProfile", "tiny");
+    rightSvg.setAttribute("viewBox", "0 0 24 24");
+    rightSvg.setAttribute("height", "5vh");
+    rightSvg.setAttribute("width", "5vh");
+    rightSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    // 创建 path 元素
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+    // 设置 path 元素的 d 属性
+    path.setAttribute("d", "M10 20c-.802 0-1.555-.312-2.122-.879-.566-.566-.878-1.32-.878-2.121s.312-1.555.879-2.122l2.878-2.878-2.878-2.879c-.567-.566-.879-1.32-.879-2.121s.312-1.555.879-2.122c1.133-1.132 3.109-1.133 4.243.001l7.121 7.121-7.122 7.121c-.566.567-1.319.879-2.121.879zm0-14c-.268 0-.518.104-.707.292-.189.19-.293.441-.293.708s.104.518.293.707l4.292 4.293-4.292 4.293c-.189.189-.293.439-.293.707s.104.518.293.707c.378.379 1.037.378 1.414.001l5.708-5.708-5.708-5.707c-.189-.189-.439-.293-.707-.293z");
+
+    // 将 path 元素添加到 rightSvg 元素中
+    rightSvg.appendChild(path);
+    // const rightButton = document.createElement('button');
+    rightSvg.setAttribute("class", "nav right");
+    rightSvg.addEventListener('click', () => {
+        if (active < imgS - 1) {
+            console.log()
+            active++;
+            setActive();
+        }
+    });
+    const Carousel = document.getElementById('carousel')
+    Carousel.appendChild(leftSvg);
+    Carousel.appendChild(rightSvg);
+    }
+  
+});
